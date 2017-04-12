45fbce748a6ddc49414456270967ac3b2e1f5f4a
Macintosh Helper <noreply@macintoshhelper.com>
2017-04-12 18:10:12 +0300

    Add authentication for addMember route

--- src/routes/addMember.js
+++ src/routes/addMember.js
@@ -2,13 +2,19 @@ const query = require('../queries/query.js');
 const Joi = require('joi');
 
 const handler = (request, reply) => {
-  query.addMember(request.payload, (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
-
-  reply.redirect('/');
+  if (request.auth.isAuthenticated) {
+    return query.addMember(request.payload, (err) => {
+      if (err) {
+        console.log(err);
+      }
+      reply.redirect('/');
+    });
+  }
+  const data = {
+    title: 'FACN Hapi Members',
+    description: 'An app which shows people involved in FACN1, where a user can see everyone involved, and add new people'
+  };
+  return reply.view('login', data);
 };
 
 const options = {
@@ -24,6 +30,10 @@ const options = {
         description: Joi.string().required(),
         languages: Joi.string().required()
       }
+    },
+    auth: {
+      mode: 'optional',
+      strategy: 'jwt'
     }
   }
 };
