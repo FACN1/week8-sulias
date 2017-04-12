15fb7931b6ad6288278e306e01ee48d4366052ee
suha-edris <suha.edris@gmail.com>
2017-04-12 11:32:07 +0300

    add githubUser db query

--- src/queries/query.js
+++ src/queries/query.js
@@ -16,4 +16,12 @@ const addMember = (member, cb) => {
   dbConnection.query('INSERT INTO members (name, position, location, description, languages) VALUES ($1, $2, $3, $4, $5)', [member.name, member.position, member.location, member.description, member.languages], cb);
 };
 
-module.exports = { getAll, searchFor, addMember };
+const addGithubUser = (user, cb) => {
+  dbConnection.query('INSERT INTO github_users (username, name, avatar_url, location, access_token) VALUES ($1, $2, $3, $4, $5)', [user.username, user.name, user.avatar_url, user.location, user.access_token], cb);
+};
+
+const getGithubUser = (username, cb) => {
+  dbConnection.query('SELECT * FROM github_users WHERE username=$1', [username], cb);
+};
+
+module.exports = { getAll, searchFor, addMember, addGithubUser, getGithubUser };
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,13 @@ const staticHandler = require('./static.js');
 const searchHandler = require('./search.js');
 const addMemberHandler = require('./addMember.js');
 const loginHandler = require('./login.js');
+const welcomeHandler = require('./welcome.js');
 
-module.exports = [homeHandler, staticHandler, addMemberHandler, searchHandler, loginHandler];
+module.exports = [
+  homeHandler,
+  staticHandler,
+  addMemberHandler,
+  searchHandler,
+  loginHandler,
+  welcomeHandler
+];
