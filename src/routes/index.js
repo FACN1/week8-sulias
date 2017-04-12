bc971c9be3f112f9ee6bb9b8abccd97b84389bdf
suha-edris <suha.edris@gmail.com>
2017-04-12 10:47:04 +0300

    add a button to login or signup on homepage

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,6 @@ const homeHandler = require('./home.js');
 const staticHandler = require('./static.js');
 const searchHandler = require('./search.js');
 const addMemberHandler = require('./addMember.js');
+const loginHandler = require('./login.js');
 
-module.exports = [homeHandler, staticHandler, addMemberHandler, searchHandler];
+module.exports = [homeHandler, staticHandler, addMemberHandler, searchHandler, loginHandler];
